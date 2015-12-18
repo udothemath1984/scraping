2e08f98adcd2ea3bda965ac8b055e09c2973c355
Tonnam <tonnam.balankura@gmail.com>
2015-12-18 12:22:40 -0500

    Add comments and line break

--- write_csv.py
+++ write_csv.py
@@ -9,8 +9,10 @@ Created on Thu Dec 03 20:02:56 2015
 import amcsd_list
 import ams_lxml
 
+# Get list of crystal structure names
 name_list = amcsd_list.get_list()
 
+# Formatting strings of .csv file for DIF files scraping
 doi = '10.1130/2006.2397(06)'
 sub_citation_1 = 'American Mineralogist Crystal Structure Database'
 url = 'http://rruff.geo.arizona.edu/AMS/amcsd.php'
@@ -22,12 +24,17 @@ unit_two_theta = '$^{\circ}$'
 list_two_theta = []
 list_intensity = []
 
+# Write .csv file in appropriate format
 with open('xray_diffraction.csv', 'w') as fout:
-    fout.write('Citation,Sub citation,Sub url,Common name,Measurement method,Measurement name,Measurement value,Measurement units,Measurement condition name,Measurement condition value,Measurement condition units\n')
+    fout.write('Citation,Sub citation,Sub url,Common name,Measurement method,\
+    Measurement name,Measurement value,Measurement units,Measurement condition name,\
+    Measurement condition value,Measurement condition units\n')
     for name in name_list[:5]:
         print name
         two_theta, intensity = ams_lxml.get_dif(name)
         list_two_theta.append(two_theta)
         list_intensity.append(intensity)
-        fout.write(doi+','+sub_citation_1+','+url+','+name+','+m_name+',Intensity,\"'+repr(intensity)+'\",'+unit_intensity+',2$\Theta$,\"'+repr(two_theta)+'\",'+unit_two_theta+'\n')
+        fout.write(doi+','+sub_citation_1+','+url+','+name+','+m_name+',Intensity,\"' \
+        +repr(intensity)+'\",'+unit_intensity+',2$\Theta$,\"'+repr(two_theta)+'\",' \
+        +unit_two_theta+'\n')
 fout.close()
