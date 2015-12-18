cb6ad87ed337c0e0a3dae7e3f7fd6a472eb3c0ab
Tonnam <tonnam.balankura@gmail.com>
2015-12-18 12:14:21 -0500

    Reformat .csv file to match Jo Hill's version

--- write_csv.py
+++ write_csv.py
@@ -11,19 +11,23 @@ import ams_lxml
 
 name_list = amcsd_list.get_list()
 
-doi = 'http://dx.doi.org/10.1130/2006.2397(06)'
+doi = '10.1130/2006.2397(06)'
+sub_citation_1 = 'American Mineralogist Crystal Structure Database'
 url = 'http://rruff.geo.arizona.edu/AMS/amcsd.php'
-m_name = 'X-ray diffraction pattern'
+m_name = 'X-ray diffraction'
+unit_intensity = 'Arb. units'
+unit_two_theta = '$^{\circ}$'
+
 
 list_two_theta = []
 list_intensity = []
 
 with open('xray_diffraction.csv', 'w') as fout:
-    fout.write('Citation,URL,Common name,Measurement name,Two theta,Intensity\n')
-    for name in name_list:
+    fout.write('Citation,Sub citation,Sub url,Common name,Measurement method,Measurement name,Measurement value,Measurement units,Measurement condition name,Measurement condition value,Measurement condition units\n')
+    for name in name_list[:5]:
         print name
         two_theta, intensity = ams_lxml.get_dif(name)
         list_two_theta.append(two_theta)
         list_intensity.append(intensity)
-        fout.write(doi+','+url+','+name+','+m_name+','+repr(two_theta)+','+repr(intensity)+'\n')
+        fout.write(doi+','+sub_citation_1+','+url+','+name+','+m_name+',Intensity,\"'+repr(intensity)+'\",'+unit_intensity+',2$\Theta$,\"'+repr(two_theta)+'\",'+unit_two_theta+'\n')
 fout.close()
