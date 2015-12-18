7851b02fe4d122fc92c5195c3f1cc60362c156c3
Tonnam <tonnam.balankura@gmail.com>
2015-12-18 13:20:49 -0500

    Add reference (Author. Journal. Title) to .csv formatting

--- write_csv.py
+++ write_csv.py
@@ -20,13 +20,12 @@ m_name = 'X-ray diffraction'
 unit_intensity = 'Arb. units'
 unit_two_theta = '$^{\circ}$'
 
-
 list_two_theta = []
 list_intensity = []
 
 # Write .csv file in appropriate format
 with open('xray_diffraction.csv', 'w') as fout:
-    fout.write('Citation,Sub citation,Sub url,Common name,Measurement method,\
+    fout.write('Citation,Sub citation,Sub citation,Sub url,Common name,Measurement method,\
     Measurement name,Measurement value,Measurement units,Measurement condition name,\
     Measurement condition value,Measurement condition units\n')
     for name in name_list[:5]:
@@ -34,7 +33,8 @@ with open('xray_diffraction.csv', 'w') as fout:
         xray_info = ams_lxml.get_xray_info(name)
         list_two_theta.append(xray_info.two_theta)
         list_intensity.append(xray_info.intensity)
-        fout.write(doi+','+sub_citation_1+','+url+','+name+','+m_name+',Intensity,\"' \
-        +repr(intensity)+'\",'+unit_intensity+',2$\Theta$,\"'+repr(two_theta)+'\",' \
-        +unit_two_theta+'\n')
+        ref_format = xray_info.author+'. '+xray_info.journal+'. '+xray_info.title
+        fout.write(doi+','+sub_citation_1+',\"'+ref_format+'\",'+url+','+name+',' \
+        +m_name+',Intensity,\"'+repr(intensity)+'\",'+unit_intensity \
+        +',2$\Theta$,\"'+repr(two_theta)+'\",'+unit_two_theta+'\n')
 fout.close()
